20cc64565d481a0a22508967330dfc65b9dcb37e
Egor Medelev <medelevep@amico.ru>
2026-03-02 14:32:46 +0300

    Добавлена кнопка "Справка" с соответствующим функционалом,добавлена защита от некорректного ввода  закрытого ключа.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Numerics;
 using System.Text;
 using System.Windows;
 using WpfApp_Decrypt_Dialogs;
+using WpfApp_Decrypt_RSA_Info;
 
 
 
@@ -37,22 +38,39 @@ namespace WpfApp_Decrypt_RSA
 				textBox_DecryptFileName.Text = _dialogService.FilePath;
 			}
 		}
+		private void button_Info_Click(object sender, RoutedEventArgs e)
+		{	
+			//Доделать текст справки
+			Info.ShowInfo();
+			return;
+		}
 		private void button_DecryptFile_Click(object sender, RoutedEventArgs e)
 		{
 			textBox_DecryptOutput.Clear();
+			String d_s = textBox_PrivateKey_d.Text;
+			String n_s = textBox_PrivateKey_n.Text;
+
 			try
 			{
 				string encFile = textBox_DecryptFileName.Text;
-				if(String.IsNullOrEmpty(textBox_DecryptFileName.Text))
+				if (String.IsNullOrEmpty(textBox_DecryptFileName.Text))
 				{
 					MessageBox.Show("Файл для расшифровки не выбран!", "Предупреждение", MessageBoxButton.OK);
 					return;
 				}
-				if(string.IsNullOrEmpty(textBox_PrivateKey_d.Text) || string.IsNullOrEmpty(textBox_PrivateKey_n.Text))
+				if (string.IsNullOrEmpty(textBox_PrivateKey_d.Text) || string.IsNullOrEmpty(textBox_PrivateKey_n.Text))
 				{
 					MessageBox.Show("Закрытый ключ не введен,или введен не полностью!", "Предупреждение", MessageBoxButton.OK);
 					return;
 				}
+				if (!ulong.TryParse(d_s, out ulong d_ul) || !ulong.TryParse(n_s,out ulong n_ul))
+				{
+                    MessageBox.Show(
+                    "Параметры  закрытого ключа RSA должны быть целыми положительными числами!",
+                   "Ошибка ввода",
+                    MessageBoxButton.OK);
+                    return;
+                }
 				BigInteger d = BigInteger.Parse(textBox_PrivateKey_d.Text);
 				BigInteger n = BigInteger.Parse(textBox_PrivateKey_n.Text);
 
